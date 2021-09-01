ae5024d2bafd4151e730b22cb10dd3565376e5d5
Arthur Pedroti <arthurpedroti@gmail.com>
2021-09-01 15:40:19 -0300

    GameCard component

---
+++ src/components/GameCard/stories.tsx
@@ -0,0 +1,47 @@
+import { Story, Meta } from '@storybook/react'
+import GameCard, { GameCardProps } from '.'
+
+export default {
+  title: 'GameCard',
+  component: GameCard,
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  },
+  argTypes: {
+    onFav: { action: 'clicked' },
+    ribbon: { type: 'string' }
+  }
+} as Meta
+
+export const Default: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
+
+Default.args = {
+  title: 'Population Zero',
+  developer: 'Rockstar Games',
+  img: 'https://source.unsplash.com/user/willianjusten/300x140',
+  price: 'R$ 235,00',
+  promotionalPrice: 'R$ 200,00'
+}
+
+export const WithRibbon: Story<GameCardProps> = (args) => (
+  <div style={{ width: '30rem' }}>
+    <GameCard {...args} />
+  </div>
+)
+
+WithRibbon.args = {
+  title: 'Population Zero',
+  developer: 'Rockstar Games',
+  img: 'https://source.unsplash.com/user/willianjusten/300x140',
+  price: 'R$ 235,00',
+  promotionalPrice: 'R$ 200,00',
+  ribbon: '20% OFF',
+  ribbonSize: 'small',
+  ribbonColor: 'primary'
+}
