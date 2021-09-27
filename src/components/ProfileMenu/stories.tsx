197fb6d373d3cea293767a0b98ae2733056adf92
Arthur Pedroti <arthurpedroti@gmail.com>
2021-09-27 07:50:46 -0300

    component: Profile Menu

---
+++ src/components/ProfileMenu/stories.tsx
@@ -0,0 +1,16 @@
+import { Story, Meta } from '@storybook/react'
+import ProfileMenu, { ProfileMenuProps } from '.'
+
+export default {
+  title: 'ProfileMenu',
+  component: ProfileMenu,
+  parameters: {
+    backgrounds: {
+      default: 'dark'
+    }
+  }
+} as Meta
+
+export const Default: Story<ProfileMenuProps> = (args) => (
+  <ProfileMenu {...args} />
+)
