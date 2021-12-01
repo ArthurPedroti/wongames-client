fd483a4b1907ec5394e446e4cdf3e5a5e98b6b68
Arthur Pedroti <arthurpedroti@gmail.com>
2021-12-01 08:09:57 -0300

    feat: installing next-auth and configure sign-up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import NextNProgress from 'nextjs-progressbar'
 
+import { Provider as AuthProvider } from 'next-auth/client'
 import { ApolloProvider } from '@apollo/client'
 import { CartProvider } from 'hooks/use-cart'
 import { ThemeProvider } from 'styled-components'
@@ -14,32 +15,34 @@ function App({ Component, pageProps }: AppProps) {
   const client = useApollo(pageProps.initialApolloState)
 
   return (
-    <ApolloProvider client={client}>
-      <ThemeProvider theme={theme}>
-        <CartProvider>
-          <Head>
-            <title>Won Games</title>
-            <link rel="shortcut icon" href="/img/icon-512.png" />
-            <link rel="apple-touch-icon" href="/img/icon-512.png" />
-            <link rel="manifest" href="/manifest.json" />
-            <meta name="theme-color" content="#06092B" />
-            <meta
-              name="description"
-              content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+    <AuthProvider session={pageProps.session}>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={theme}>
+          <CartProvider>
+            <Head>
+              <title>Won Games</title>
+              <link rel="shortcut icon" href="/img/icon-512.png" />
+              <link rel="apple-touch-icon" href="/img/icon-512.png" />
+              <link rel="manifest" href="/manifest.json" />
+              <meta name="theme-color" content="#06092B" />
+              <meta
+                name="description"
+                content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
+              />
+            </Head>
+            <GlobalStyles />
+            <NextNProgress
+              color={theme.colors.primary}
+              startPosition={0.3}
+              stopDelayMs={200}
+              height={3}
+              showOnShallow={true}
             />
-          </Head>
-          <GlobalStyles />
-          <NextNProgress
-            color={theme.colors.primary}
-            startPosition={0.3}
-            stopDelayMs={200}
-            height={3}
-            showOnShallow={true}
-          />
-          <Component {...pageProps} />
-        </CartProvider>
-      </ThemeProvider>
-    </ApolloProvider>
+            <Component {...pageProps} />
+          </CartProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    </AuthProvider>
   )
 }
 
